58a2d3406a8cb7f86d328e80ddd4edb20c8d0696
dudekdawid89 <dudekdawid89@gmail.com>
2020-01-05 13:55:57 -0600

    Webpack server set up and .large-hero block style

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,16 @@ module.exports = {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
+    devServer: {
+        before: function(app, server){
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
     mode: 'development',
-    watch: true, // this is an -Webpack- option to watch every changes in the code
     module: {
         rules: [
             {
